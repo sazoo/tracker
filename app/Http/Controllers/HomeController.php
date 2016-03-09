e81a3da1cd4d2ab51215f317005994598669570c
Deo Al Emboltorio <daae25@gmail.com>
2016-03-09 11:38:28 +0800

    added home page

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Ticker;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+class HomeController extends Controller
+{
+    public function showDashboard(){		
+		$tickers = Ticker::all()->take(24)->get();
+		
+		
+	}
+}
