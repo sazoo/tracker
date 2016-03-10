c2a2fa54985f10f1ff7df2c3e890fda616c7840f
Deo Al Emboltorio <daae25@gmail.com>
2016-03-10 23:08:37 +0800

    added data

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,8 +11,16 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function showDashboard(){		
-		$tickers = Ticker::all()->take(24)->get();
+		$tickersArr = Ticker::orderBy('created_at', 'ASC')->take(24)->get()->toArray();
 		
-		
+		$i = 0;
+		$data = array();
+		foreach($tickersArr as $ticker){
+			$data[$i]['date'] = $ticker['created_at'];
+			$data[$i]['buy_current'] = $ticker['bid'];
+			$data[$i]['sell_current'] = $ticker['ask'];
+		$i++;
+		}
+		return view('home')->with('data', $data);
 	}
 }
