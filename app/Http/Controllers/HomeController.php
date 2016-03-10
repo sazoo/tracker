1d4d0542dcf5b5e2c17de43f8a363b0971ce8c63
Deo Al Emboltorio <daae25@gmail.com>
2016-03-11 02:11:54 +0800

    fixed

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -11,25 +11,28 @@ use App\Http\Controllers\Controller;
 class HomeController extends Controller
 {
     public function showDashboard(){		
-		$tickersArr = Ticker::orderBy('created_at', 'ASC')->take(25)->get()->toArray();
+		$tickersArr = Ticker::orderBy('created_at', 'ASC')->take(24)->get()->toArray();
 		
 		$i = 0;
 		$data = array();
 		foreach($tickersArr as $ticker){
-			if($i >= 1){
-				$data[$i-1]['date'] = $ticker['created_at'];
-				$data[$i-1]['buy_current'] = $ticker['bid'];
-				$data[$i-1]['sell_current'] = $ticker['ask'];
-				$data[$i-1]['buy_four_ema'] = $ticker['buy_four_ema'];
-				$data[$i-1]['sell_four_ema'] = $ticker['sell_four_ema'];
+				$data[$i]['date'] = $ticker['created_at'];
+				$data[$i]['buy_current'] = $ticker['bid'];
+				$data[$i]['sell_current'] = $ticker['ask'];
+				$data[$i]['buy_four_ema'] = $ticker['buy_four_ema'];
+				$data[$i]['sell_four_ema'] = $ticker['sell_four_ema'];
 				
 				//compute 4-EMA diff
-				$diffBuy = (($prevBuyFourEMA - $ticker['buy_four_ema'])*100)/$prevBuyFourEMA;
-				$diffSell = (($prevBuyFourEMA - $ticker['buy_four_ema'])*100)/$prevBuyFourEMA;
-				
-				$data[$i-1]['buy_four_prcnt_diff'] = $diffBuy;
-				$data[$i-1]['sell_four_prcnt_diff'] = $diffSell;
-			}
+				if($tickersArr->size() >= 4){
+					$diffBuy = (($prevBuyFourEMA - $ticker['buy_four_ema'])*100)/$prevBuyFourEMA;
+					$diffSell = (($prevBuyFourEMA - $ticker['buy_four_ema'])*100)/$prevBuyFourEMA;
+					
+					$data[$i]['buy_four_prcnt_diff'] = $diffBuy;
+					$data[$i]['sell_four_prcnt_diff'] = $diffSell;
+				}else{
+					$data[$i]['buy_four_prcnt_diff'] = '';
+					$data[$i]['sell_four_prcnt_diff'] = '';
+				}
 			$prevBuyFourEMA = $ticker['buy_four_ema'];
 			$prevSellFourEMA = $ticker['sell_four_ema'];
 		$i++;
