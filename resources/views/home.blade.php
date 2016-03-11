a9d5228eaf99aa7c51c4643150d6d00d9c8963b9
Deo Al Emboltorio <daae25@gmail.com>
2016-03-11 22:23:18 +0800

    gf

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,9 +5,6 @@
 		<meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
 		<style>
-			table thead {
-			  position: fixed;
-			}
 			.flat-table {
 			  display: block;
 			  font-family: sans-serif;
